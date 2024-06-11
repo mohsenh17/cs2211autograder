28d057f1323e03cfcdabcf4883f991fa4a70857b
SeyedMohsen Hosseini <shosse59@uwo.ca>
2024-06-11 10:32:30 -0400

    Reorganize project structure

--- run_autograder.sh
+++
@@ -1,8 +0,0 @@
-﻿#!/bin/bash
-
-base_dir="assignment2/"
-directories=$(find $base_dir -maxdepth 1 -type d)
-for dir in $directories; do
-python q1_autograder.py $dir
-python q2_autograder.py $dir
-done
